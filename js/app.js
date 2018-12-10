836cc5007109075e46589335d825c72e69f4bfbc
Corey <corey.aing@tiger-sheep.com>
2018-12-09 23:33:50 -0800

    Finish implementation of functions

--- js/app.js
+++ js/app.js
@@ -58,17 +58,15 @@ var App = {
 						name: result.name,
 						countryName: result.countryName,
 						lat: result.lat,
-						lng: result.lng
+						lng: result.lng,
+						title: `${result.name}, ${result.countryName}`
 					};
-					newLink = newLink + `<a href="javascript:App.user.reCenter(${locationPayload});">${result.name}, ${result.countryName}</a><br>`;
+					newLink = newLink + `<a href="javascript:App.user.reCenter(${locationPayload});">${locationPayload.title}</a><br>`;
 				} else {
 					newLink = newLink + `<a href="javascript:App.user.reCenter(${locationPayload});">${result.name}</a><br>`;
 				}
 				document.getElementById('searchResult').innerHTML = newLink;
 			}
-		},
-		reCenter: (locPayload) => {
-			
 		}
 	},
 	
@@ -95,6 +93,95 @@ var App = {
 			});
 			App.mapData.setMainMap(newMap);
 			App.mapData.setBoundingBox(newBoundingBox);
+		},
+		center: (locPayload) => {
+			let point = {
+				lat: locPayload.lat,
+				lng: locPayload.lng
+			};
+			let map = App.mapData.getMainMap();
+			
+			map.setCenter(point);
+			App.mapData.deleteMarkers();
+			map.setZoom(5);
+			document.getElementById('currentView').innerHTML = `<b>Origin of resulting view: </b> ${locPayload.title}`;
+			//addMarker()
+			
+			let boundary = {
+				north: locPayload.lat + 5,
+				south: locPayload.lat - 5,
+				east: locPayload.lng + 10,
+				west: locPayload.lng - 10
+			};
+			//searchRecentEarthquake(boundary);
+		},
+		searchRecentEarthquakes: (boundary) => {
+			let req = `http://api.geonames.org/earthquakesJSON?north=${boundary.north}&south=${boundary.south}&east=${boundary.east}&west=${boundary.west}&username=caing`;
+			let boundingBox = App.mapData.getBoundingBox();
+			boundingBox.setBounds(boundary);
+			boundingBox.setMap(App.mapData.getMainMap());
+			
+			return $.ajax({
+				'type': 'GET',
+				'url': req,
+				'dataType': 'json'
+			});
+		},
+		getEarthquakeInfo: (payload) => {
+			if (payload === null || payload.earthquakes < 1) {
+				alert('No recent earthquakes in this area');
+			} else {
+				let quakeInfo = payload.earthquakes;
+				for (let i = 0; i < quakeInfo.length; i++) {
+					let result = quakeInfo[i];
+					let earthquakeInfo = {
+						lat: result.lat,
+						lng: result.lng,
+						magnitude: result.magnitude,
+						datetime: result.datetime,
+						depth: result.depth
+					};
+					//addEQMarker(earthquakeInfo);
+				}
+			}
+		},
+		addEQMarker: (earthquakeInfo) => {
+			let markerDetail = `
+				<div class="uk-card uk-card-default uk-card-small" name="markerDetail">
+					<ul class="uk-list uk-list-divider">
+						<li><b>Magnitude of Earthquake: </b> ${earthquakeInfo.magnitude}</li>
+						<li><b>Latitude: ${earthquakeInfo.lat}</b></li>
+						<li><b>Longitude: ${earthquakeInfo.lng}</b></li>
+						<li><b>Depth: ${earthquakeInfo.depth}</b></li>
+						<li>(Happened on ${earthquakeInfo.datetime})</li>
+					</ul>
+				</div>
+			`;
+			
+			let detailWindow = new google.maps.InfoWindow({content: markerDetail});
+			let newMarker = new google.maps.Marker({
+				position: {lat: earthquakeInfo.lat, lng: earthquakeInfo.lng},
+				map: App.mapData.getMainMap(),
+				title: 'Happened on ' + earthquakeInfo.datetime
+			});
+			
+			newMarker.addListener('click', () => {
+				detailWindow.open(App.mapData.getMainMap(), newMarker);
+			});
+			App.mapData.pushMarkers(newMarker);
+		},
+		addMarker: (point, name) => {
+			let newMarker = new google.maps.Marker({
+				position: point,
+				icon: {
+					path: google.maps.SymbolPath.BACKWARDS_CLOSED_ARROW,
+					strokeColor: '#FFFF00',
+					scale: 2
+				},
+				map: App.mapData.getMainMap(),
+				title: name
+			});
+			App.mapData.pushMarkers(newMarker);
 		}
 	}
 }
